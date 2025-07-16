ee448f051710b9627a84d6386fe979b83043618e
Jeremy King <jking@oilygears.com>
2025-07-16 09:05:19 -0400

    update to allow parameters

--- flats/line-readers/line_reader_mongodb.sh
+++ flats/line-readers/line_reader_mongodb.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
 # Example: Import a delimited file (pipe-delimited by default, or any delimiter) into MongoDB using mongoimport
-# Usage: ./line_reader_mongodb.sh [filename] [delimiter]
+
+# Usage: ./line_reader_mongodb.sh [filename] [delimiter] [db] [collection]
 # Default filename: flatfile_data.txt
 # Default delimiter: |
+# Default db: natcrim
+# Default collection: sor_data
+
 
 FILE="${1:-flatfile_data.txt}"
 DELIM="${2:-|}"
+DB="${3:-natcrim}"
+COLLECTION="${4:-sor_data}"
 
 
 # mongoimport expects a CSV, so we convert the delimiter if needed
@@ -16,8 +22,9 @@ if [ "$DELIM" != "," ]; then
     FILE="$TMPFILE"
 fi
 
-# Import into MongoDB (edit --db and --collection as needed)
-mongoimport --type csv --headerline --db testdb --collection flatfile --file "$FILE"
+
+# Import into MongoDB
+mongoimport --type csv --headerline --db "$DB" --collection "$COLLECTION" --file "$FILE"
 
 if [ -n "$TMPFILE" ]; then
     rm "$TMPFILE"
